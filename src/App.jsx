fe95b07457183409b8ec9d73426a1acbde76cbe6
carkotx <carkotx@gmail.com>
2022-09-22 18:29:18 -0300

    navbardesafio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css'
+import Saludo from './components/Saludo';
 
 // SI quiero agregar un componente afuera de navbar lo hacemos aqui
 // debajo de <Navbar/>
@@ -9,7 +10,7 @@ const App = () => {
     <>
   
       <Navbar/>
-  
+      <Saludo/>
    </>
 
    );
---
+++ src/components/Saludo.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const Saludo = () => {
+    return (
+        <Fragment>
+
+       <h2>hola juan</h2>
+
+        </Fragment>
+    );
+}
+
+export default Saludo;
